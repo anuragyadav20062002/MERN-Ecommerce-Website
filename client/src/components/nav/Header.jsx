06e4557d6e653449af1659fac44e8109039972ee
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-18 22:48:26 +0530

    added userselector to show selected links when login and logout

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -14,7 +14,7 @@ import { Link } from "react-router-dom"
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router-dom"
 
 const { SubMenu, Item } = Menu
@@ -22,6 +22,7 @@ const { SubMenu, Item } = Menu
 const Header = () => {
   const [current, setCurrent] = useState("home")
   let dispatch = useDispatch()
+  let { user } = useSelector((state) => ({ ...state }))
   let history = useHistory()
 
   const handleClick = (e) => {
@@ -47,20 +48,26 @@ const Header = () => {
         <Item key="home" icon={<AppstoreOutlined />}>
           <Link to="/">Home</Link>
         </Item>
-        <Item key="register" className="float-end" icon={<UserAddOutlined />}>
-          <Link to="/register">Register</Link>
-        </Item>
-        <Item key="login" className="float-end" icon={<UserOutlined />}>
-          <Link to="/login">Login</Link>
-        </Item>
-
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
-          <Item key="setting:1">Option 1 </Item>
-          <Item key="setting:2">Option 2</Item>
-          <Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
-            Logout
+        {!user && (
+          <Item key="register" className="float-end" icon={<UserAddOutlined />}>
+            <Link to="/register">Register</Link>
           </Item>
-        </SubMenu>
+        )}
+        {!user && (
+          <Item key="login" className="float-end" icon={<UserOutlined />}>
+            <Link to="/login">Login</Link>
+          </Item>
+        )}
+
+        {user && (
+          <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+            <Item key="setting:1">Option 1 </Item>
+            <Item key="setting:2">Option 2</Item>
+            <Item key="logout" icon={<LogoutOutlined />} onClick={logout}>
+              Logout
+            </Item>
+          </SubMenu>
+        )}
       </Menu>
     </>
   )
