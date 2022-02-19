1e752ad2a8a8839d32f812755295bd4df08198ed
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-19 16:22:27 +0530

    added controller folder to store all route functions

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/create-or-update-user", (req, res) => {
-  res.json({
-    data: "hey you hit create-or-update-user api endoint",
-  })
-})
+//import
+
+const { createOrUpdateUser } = require("../controllers/auth")
+
+router.get("/create-or-update-user", createOrUpdateUser)
 
 module.exports = router
