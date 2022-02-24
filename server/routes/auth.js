9c7e33eebbacb2dcc9dc7723700babaed8e93ccc
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-24 18:26:26 +0530

    initialized auth check middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-//import
+//middleware
+
+const { authCheck } = require("../middlewares/auth")
+
+//controller
 
 const { createOrUpdateUser } = require("../controllers/auth")
 
-router.get("/create-or-update-user", createOrUpdateUser)
+router.post("/create-or-update-user", authCheck, createOrUpdateUser)
 
 module.exports = router
